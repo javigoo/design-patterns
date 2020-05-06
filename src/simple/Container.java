adf645fc5f4c51333403dbe7b23c48d8d637f438
jal04 <josemi.bnf@outlook.es>
2020-05-06 10:51:59 +0000

    getObject for Factory and Singleton

--- src/simple/Container.java
+++ src/simple/Container.java
@@ -9,19 +9,21 @@ public class Container implements Injector{
     
     private HashMap<String,Object> registeredObjects;
     private HashMap<String,simple.Factory> FactoriesMap;
+    private HashMap<String,simple.Factory> SingletonMap;
     private HashMap<String,String[]> dependencesMap;
     private boolean DEBUG = true;
 
     public Container(){
         this.registeredObjects = new HashMap<>();
         this.FactoriesMap = new HashMap<>();
+        this.SingletonMap = new HashMap<>();
         this.dependencesMap = new HashMap<>();
     }
 
     public void registerConstant(String name, Object value) throws DependencyException {
         // Associa el nom al valor, de manera que quan es demani getObject donat el nom,
         // es retornarà aquest valor
-        if (this.registeredObjects.containsKey(name) || this.FactoriesMap.containsKey(name)){  // Comprueba si el nombre se encuentra en algon HashMap
+        if (this.registeredObjects.containsKey(name) || this.FactoriesMap.containsKey(name) || this.SingletonMap.containsKey(name)){  // Comprueba si el nombre se encuentra en algon HashMap
             if (DEBUG) System.err.println("ERROR: '" + name + "' constant is already registered.");
             throw new DependencyException(name + " constant is already registered.");
         } else{
@@ -31,7 +33,7 @@ public class Container implements Injector{
     }
 
     public void registerFactory(String name, Factory creator, String... parameters) throws DependencyException {
-        if (this.FactoriesMap.containsKey(name) || this.registeredObjects.containsKey(name)){  // Comprueba si el nombre se encuentra en algon HashMap
+        if (this.FactoriesMap.containsKey(name) || this.registeredObjects.containsKey(name) || this.SingletonMap.containsKey(name)){  // Comprueba si el nombre se encuentra en algon HashMap
             if (DEBUG) System.err.println("ERROR: '" + name + "' factory is already registered.");
             throw new DependencyException(name + " factory is already registered.");
         }else{
@@ -59,6 +61,18 @@ public class Container implements Injector{
         // Fixeu-vos que en el moment de fer l’enregistrament no podem crear la
         // instància, doncs podria ser que no
         // totes les dependències estiguin ja enregistrades.
+
+        if (this.FactoriesMap.containsKey(name) || this.registeredObjects.containsKey(name) || this.SingletonMap.containsKey(name)){  // Comprueba si el nombre se encuentra en algon HashMap
+            if (DEBUG) System.err.println("ERROR: '" + name + "' factory is already registered.");
+            throw new DependencyException(name + " factory is already registered.");
+        }else{
+            if (DEBUG) System.out.println("Trying to register a factory like Singleton with name: '" + name + "'");
+            this.SingletonMap.put(name, creator);
+            if (DEBUG) System.out.println("Successfull factory register with FactoryName: '" + name + "'");
+            if (DEBUG) System.out.println("Trying to register a factory dependences with FactoryName: '" + name + "'");
+            this.dependencesMap.put(name, parameters);
+            if (DEBUG) System.out.println("Successfull dependences register for Factory: '" + name + "'");
+        }
     }
 
     public Object getObject(String name) throws DependencyException {
@@ -66,10 +80,48 @@ public class Container implements Injector{
             return this.registeredObjects.get(name);
         }
         else if(this.FactoriesMap.containsKey(name)){
-            return this.FactoriesMap.get(name);            
+            return this.makeFactory(name);            
+        }
+        else if(this.SingletonMap.containsKey(name)){
+            return this.getSingletonFactory(name);            
         }
         else{
             throw new DependencyException(name + " has not been registered.");
         }        
+    }
+
+    private Object getSingletonFactory(String name) throws DependencyException {
+        try{
+            if (instance == null){
+                Factory creator;
+                creator = this.FactoriesMap.get(name);
+                Object[] str1 = new Object[this.dependencesMap.get(name).length];
+                for (int i=0; i<this.dependencesMap.get(name).length; i++){
+                    str1[i] = this.getObject(this.dependencesMap.get(name)[i]);
+                }
+                return creator.create(str1);
+            }else{
+                return instance;
+            }
+            
+        }catch(DependencyException ex){
+                if (DEBUG) System.err.println("ERROR: Something whent wrong trying to make '" + name + "' factory");
+                throw new DependencyException(ex);
+        }
+    }
+
+    private Object makeFactory(String name) throws DependencyException { // Cada vez que pedimos ese nombre, nos crea una instancia de Factory.
+        try{
+            Factory creator;
+            creator = this.FactoriesMap.get(name);
+            Object[] str1 = new Object[this.dependencesMap.get(name).length];
+            for (int i=0; i<this.dependencesMap.get(name).length; i++){
+                str1[i] = this.getObject(this.dependencesMap.get(name)[i]);
+            }
+            return creator.create(str1);
+        }catch(DependencyException ex){
+                if (DEBUG) System.err.println("ERROR: Something whent wrong trying to make '" + name + "' factory");
+                throw new DependencyException(ex);
+        }
     }
 }
