2a66b98280f2546ab3a5959c7f26becdb249d8d4
jal04 <josemi.bnf@outlook.es>
2020-05-06 15:10:44 +0000

    deleted Singleton.java, que no vale XD

--- src/simple/Singleton.java
+++
@@ -1,8 +0,0 @@
-package simple;
-
-import common.DependencyException;
-
-public interface Singleton extends Factory {
-    Object factory;
-    Object create(Object... parameters) throws DependencyException;
-}
