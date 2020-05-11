b629821e2ca8a4892d231db22f609d6b91e6605d
jal04 <josemi.bnf@outlook.es>
2020-05-11 15:07:11 +0200

    simple tests passed 1

--- test/simple/ContainerTest.java
+++ test/simple/ContainerTest.java
@@ -78,6 +78,52 @@ public class ContainerTest {
         assertThat(d1.getI(), is(42));
     }
 
+    @Test
+    public void instanceOfFactoryA1Singleton() throws DependencyException{
+        registerConstants();
+        registerSingletons();
+
+        InterfaceA a = (InterfaceA) injector.getObject("InterfaceA");
+        assertThat(a, is(instanceOf(ImplementationA1.class)));
+        ImplementationA1 a1 = (ImplementationA1) a;
+        assertThat(a1.b, is(instanceOf(ImplementationB1.class)));
+        assertThat(a1.c, is(instanceOf(ImplementationC1.class)));
+    }
+
+    @Test
+    public void instanceOfFactoryB1Singleton() throws DependencyException{
+        registerConstants();
+        registerSingletons();
+
+        InterfaceB b = (InterfaceB) injector.getObject("InterfaceB");
+        assertThat(b, is(instanceOf(ImplementationB1.class)));
+        ImplementationB1 b1 = (ImplementationB1) b;
+        assertThat(b1.d, is(instanceOf(ImplementationD1.class)));
+    }
+
+    @Test
+    public void instanceOfFactoryC1Singleton() throws DependencyException{
+        registerConstants();
+        registerSingletons();
+
+        InterfaceC c = (InterfaceC) injector.getObject("InterfaceC");
+        assertThat(c, is(instanceOf(ImplementationC1.class)));
+        ImplementationC1 c1 = (ImplementationC1) c;
+        assertThat(c1.s,is("El sentido de la vida, el universo y todo lo demas"));
+    }
+
+    @Test
+    public void instanceOfFactoryD1Singleton() throws DependencyException{
+        registerConstants();
+        registerSingletons();
+
+        InterfaceD d = (InterfaceD) injector.getObject("InterfaceD");
+        assertThat(d, is(instanceOf(ImplementationD1.class)));
+        ImplementationD1 d1 = (ImplementationD1) d;
+        assertThat(d1.i, is(42));
+    }
+
+
     @Test(expected = DependencyException.class)
     public void alreadyRegisteredConstant() throws DependencyException{
         injector.registerConstant("Integer", 0);
@@ -102,12 +148,63 @@ public class ContainerTest {
     }
 
     @Test(expected = DependencyException.class)
+    public void getObjectNotRegisteredDependenciesFactory() throws DependencyException{
+        injector.registerFactory("InterfaceD", new simple.factories.FactoryD1(), "Integer");
+
+        injector.getObject("InterfaceD");
+    }
+
+    @Test(expected = DependencyException.class)
+    public void getObjectNotRegisteredDependenciesSingleton() throws DependencyException{
+        injector.registerSingleton("InterfaceD", new simple.factories.FactoryD1(), "Integer");
+
+        injector.getObject("InterfaceD");
+    }
+
+    @Test(expected = DependencyException.class)
     public void registerFactoryWithBadArguments() throws DependencyException{
         injector.registerConstant("Integer", 0);
         injector.registerFactory("InterfaceD", new FactoryD1(), "String");
         injector.getObject("InterfaceD");
     }
 
+
+    @Test(expected = DependencyException.class)
+    public void registerSingletonWithBadArguments() throws DependencyException{
+        injector.registerConstant("Integer", 0);
+        injector.registerSingleton("InterfaceD", new simple.factories.FactoryD1(), "String");
+
+        injector.getObject("InterfaceD");
+    }
+
+    @Test(expected = DependencyException.class)
+    public void getObjectWithDependencyCycle() throws DependencyException{
+        injector.registerFactory("InterfaceA", new simple.factories.FactoryA1(), "InterfaceB", "InterfaceC");
+        injector.registerFactory("InterfaceD", new simple.factories.FactoryD1(), "InterfaceA");
+        injector.registerSingleton("InterfaceC", new simple.factories.FactoryC1(), "InterfaceD");
+
+        injector.getObject("InterfaceA");
+    }
+
+
+    private void registerConstants() throws DependencyException {
+        injector.registerConstant("Integer", 42);
+        injector.registerConstant("String", "El sentido de la vida, el universo y todo lo demas");
+    }
+
+    private void registerFactories() throws DependencyException {
+        injector.registerFactory("InterfaceA", new simple.factories.FactoryA1(), "InterfaceB", "InterfaceC");
+        injector.registerFactory("InterfaceB", new simple.factories.FactoryB1(), "InterfaceD");
+        injector.registerFactory("InterfaceC", new simple.factories.FactoryC1(), "String");
+        injector.registerFactory("InterfaceD", new simple.factories.FactoryD1(), "Integer");
+    }
+
+    private void registerSingletons() throws DependencyException {
+        injector.registerSingleton("InterfaceA", new simple.factories.FactoryA1(), "InterfaceB", "InterfaceC");
+        injector.registerSingleton("InterfaceB", new simple.factories.FactoryB1(), "InterfaceD");
+        injector.registerSingleton("InterfaceC", new simple.factories.FactoryC1(), "String");
+        injector.registerSingleton("InterfaceD", new simple.factories.FactoryD1(), "Integer");
+    }
 
     // TODO
     // Repetir test para cada factory (ok)
