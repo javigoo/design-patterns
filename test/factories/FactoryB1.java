d4af3c7426cff7d05b779076478eee05ed8f8332
jal04 <josemi.bnf@outlook.es>
2020-05-11 13:24:15 +0200

    factory en simple sin <I>

---
+++ test/factories/FactoryB1.java
@@ -0,0 +1,20 @@
+package factories;
+
+import common.DependencyException;
+import simple.Factory;
+import implementations.ImplementationB1;
+import implementations.ImplementationD1;
+import interfaces.InterfaceD;
+
+public class FactoryB1 implements Factory<ImplementationB1> {
+    @Override
+    public ImplementationB1 create(Object... parameters) throws DependencyException {
+        InterfaceD d;
+        try {
+            d = (ImplementationD1) parameters[0];
+        } catch (ClassCastException | ArrayIndexOutOfBoundsException ex) {
+            throw new DependencyException(ex);
+        }
+        return new ImplementationB1(d);
+    }
+}
---
+++ test/factories/FactoryD1.java
@@ -0,0 +1,18 @@
+package factories;
+
+import common.DependencyException;
+import simple.Factory;
+import implementations.ImplementationD1;
+
+public class FactoryD1 implements Factory <ImplementationD1> {
+    @Override
+    public ImplementationD1 create(Object... parameters) throws DependencyException {
+        int i;
+        try {
+            i = (int) parameters[0];
+        } catch (ClassCastException | ArrayIndexOutOfBoundsException ex) {
+            throw new DependencyException(ex);
+        }
+        return new ImplementationD1(i);
+    }
+}
